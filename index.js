871f704de5c60f0cacf2e380c6e2f4fcee6a821c
truptikolte <tnkoltegmail.com>
2022-01-31 12:32:29 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//document.getElementById("count-el").innerText = 5;
+let count1 = 3
+console.log(count1);
+
+let myAge = 43
+let humanDogRatio = 7
+myDogAge = myAge * humanDogRatio;
+console.log(myDogAge);
